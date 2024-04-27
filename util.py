96a943a9ac1d8c520caae05fbece0e23da8a1a48
imradhe <radhesyam.musicals@gmail.com>
2024-04-27 21:29:00 +0530

    Updated uitl

--- util.py
+++ util.py
@@ -3,13 +3,12 @@
 import time
 import random
 import string
-import nltk
-from nltk.tokenize import sent_tokenize
 import re
 import nltk
+from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from nltk.stem import PorterStemmer
+from nltk.stem.porter import *
+from nltk.stem import PorterStemmer, WordNetLemmatizer
 import numpy as np
 from scipy import stats
 import os
