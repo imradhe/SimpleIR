6fcea1c961e9c48947b411460f3cb12ab7028a6e
imradhe <radhesyam.musicals@gmail.com>
2024-04-27 21:28:54 +0530

    Updated Inflection Reduction

--- inflectionReduction.py
+++ inflectionReduction.py
@@ -3,41 +3,40 @@ from util import *
 # Add your import statements here
 
 
-
-
 class InflectionReduction:
-    def reduce(self, text):
-        """
-        Stemming/Lemmatization
+	def reduce(self, text, approach = "stem"):
+		return self.stem(text) if approach == "stem" else self.lemmatize(text) if approach == "lemmatize" else self.hybrid(text)
+	
+	def stem(self, text):
+		reducedText = None
+		"""
+		Stemming/Lemmatization
 
-        Parameters
-        ----------
-        text : list
-            A list of lists where each sub-list is a sequence of tokens
-            representing a sentence
+		Parameters
+		----------
+		arg1 : list
+			A list of lists where each sub-list a sequence of tokens
+			representing a sentence
 
-        Returns
-        -------
-        list
-            A list of lists where each sub-list is a sequence of
-            stemmed/lemmatized tokens representing a sentence
-        """
-        reducedText = []
-
-        lemmatizer = WordNetLemmatizer()
-        stemmer = PorterStemmer()
-
-        for sentence_tokens in text:
-            # Lemmatize each token
-            lemmatized_tokens = [lemmatizer.lemmatize(token) for token in sentence_tokens]
-            # Stem each token
-            stemmed_tokens = [stemmer.stem(token) for token in sentence_tokens]
-            reducedText.append((lemmatized_tokens, stemmed_tokens))
-            
-        return reducedText
-
-# # Example usage
-# inflection_reducer = InflectionReduction()
-# tokenized_sentences = [["running", "walked"], ["dogs", "cats"], ['singer', 'liar'], ['locating' , 'laying']]
-# reduced_sentences = inflection_reducer.reduce(tokenized_sentences)
-# print(reduced_sentences)
+		Returns
+		-------
+		list
+			A list of lists where each sub-list is a sequence of
+			stemmed/lemmatized tokens representing a sentence
+		"""
+		#Fill in code here
+		reducedText = [[PorterStemmer().stem(word) for word in sentence]for sentence in text]
+		return reducedText
+	
+	def lemmatize(self, text):
+		reducedText = None
+		#Fill in code here
+		reducedText= [[WordNetLemmatizer().lemmatize(word) for word in sentence]for sentence in text]
+		return reducedText
+	
+	def hybrid(self, text):
+		reducedText = None
+		#Fill in code here
+		reducedText= [[WordNetLemmatizer().lemmatize(word) for word in sentence]for sentence in text]
+		reducedText= [[PorterStemmer().stem(word) for word in sentence]for sentence in reducedText]
+		return reducedText
