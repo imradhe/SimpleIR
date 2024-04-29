791dede7aaa92a087d1dbe86431df3e20b1342ab
imradhe <radhesyam.musicals@gmail.com>
2024-04-29 22:55:51 +0530

    Fixed Inflection Reduction

--- inflectionReduction.py
+++ inflectionReduction.py
@@ -1,4 +1,7 @@
 from util import *
+import time
+import random
+import string
 
 # Add your import statements here
 
@@ -81,4 +84,30 @@ class InflectionReduction:
         }
 
 		return metrics
+	def efficiency(self, num_sentences):
+		results = {
+			'stem_times': [],
+			'lemmatize_times': [],
+			'hybrid_times': []
+		}
+		for num in num_sentences:
+			exec_time_stem = self.exec_time(num, "stem")
+			exec_time_lemmatize = self.exec_time(num, "lemmatize")
+			exec_time_hybrid = self.exec_time(num, "hybrid")
+			results['stem_times'].append(exec_time_stem)
+			results['lemmatize_times'].append(exec_time_lemmatize)
+			results['hybrid_times'].append(exec_time_hybrid)
+		return results
 
+
+	def exec_time(self, num_sentences, approach = 'stem'):
+		text = []
+		for _ in range(num_sentences):
+			num_tokens = random.randint(10 - 2, 10 + 2)  # Vary tokens per sentence
+			sentence = [random.choice(string.ascii_lowercase) for _ in range(num_tokens)]
+			text.append(sentence)
+		startTime = time.time()
+		self.reduce(text, approach)
+		endTime = time.time()
+		return endTime - startTime
+	
